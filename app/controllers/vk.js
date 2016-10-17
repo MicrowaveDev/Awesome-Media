817c3fa9b52435240fa1fbea23252548e324bd4e
Jonybang <jonybang@mail.ru>
2016-10-17 22:38:27 +1000

    try to fix not first vk sync

--- app/controllers/vk.js
+++ app/controllers/vk.js
@@ -281,19 +281,16 @@ module.exports = {
                     //END ALL TASKS
 
                     User.findOne({_id: session.user_id}, function(err, dbUser) {
-						user = dbUser;
 
 						let media_ids = commonHelper.getKeysSortedByValue(media_ids_dict);
-						_.forEach(user.medias, (user_media, index) => {
+						_.forEach(dbUser.medias, (user_media, index) => {
 							if(!user_media.number && user_media.source == 'vk'){
-								user_media.number = media_ids.indexOf(user_media.media_id.toString());
-								delete user_media._id;
-								user.medias.set(index, user_media);
+								dbUser.medias[index].number = media_ids.indexOf(user_media.media_id.toString());
+								console.log('user.medias[index].number ', dbUser.medias[index].number)
 							}
 						});
 
-						user.markModified('medias');
-						user.save();
+						dbUser.save();
 
 						socket.emit('all_success', apiHelper.socketResponse(err, "VK Audio Sync complete", result));
 						socket.disconnect(0);
