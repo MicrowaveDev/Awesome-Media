4cc31834f6be5d3f13389a1d5ac0da9017cd826a
Graver75 <undergraver75@gmail.com>
2016-11-03 22:59:35 +0300

    useless changes

--- app/models/playlist.js
+++ app/models/playlist.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose'),
 
 const Playlist = mongoose.model('Playlist', {
     name: String,
-    content: Array
+    content: Array 
 });
 
 module.exports = Playlist;
--- app/routes.js
+++ app/routes.js
@@ -20,6 +20,7 @@ module.exports = function(app){
     app.post("/api/media", mediaController.postMedia);
     app.post('/api/media-upload', commonController.prepareTestUser, mediaController.uploadMedia);
 
+
     // app.get('/api/test_env', function(req, res){
     //     res.status(200).json(process.env);
     // });
