35e78e4cb71da9af1cd505aaa4fa6860b30c5897
Graver75 <undergraver75@gmail.com>
2016-11-30 21:05:27 +0300

    added bugged test

---
+++ app/tests/tests.js
@@ -0,0 +1,38 @@
+/**
+ * Created by graver on 29.11.16.
+ */
+"use strict";
+const _ = require('lodash');
+const request = require('request');
+const expect = require('chai').expect;
+
+const User = require('./../models/user');
+
+const apiHelper = require('./../helpers/api');
+const playlistsController = require('./../controllers/playlists');
+const commonController = require('./../controllers/common');
+
+
+let requestRaw = {
+    session: null
+};
+
+describe('Routes', function () {
+    describe('Get lists', function () {
+        before(function () {
+            User.find({}, function (err, docs) {
+                if (docs && docs.length) {
+
+                }
+            })
+        })
+
+        let url = "http://localhost:3007/api/playlists";
+        it("Should return playlists", function (done) {
+            request(url, (err, res, body) => {
+                expect(res.statusCode).to.equal(200); //but it equals 400!!! user not found!!
+                done();
+            })
+        });
+    });
+});
