40c6c04455f33dea772179342ab830cb44ee52be
Graver75 <undergraver75@gmail.com>
2016-12-02 00:36:12 +0300

    added each-blocks

--- app/tests/tests.js
+++ app/tests/tests.js
@@ -16,26 +16,41 @@ const commonController = require('./../controllers/common');
 let requestRaw = {
     session: null
 };
+let user_id;
 
 describe('Routes', function () {
     describe('Get lists', function () {
-        before(function () {
-            User.find({}, function (err, docs) {
-                if (docs && docs.length) {
-                    //give me the connect.sid
+        beforeEach('Creates test user', function () {
+            let user = new User({
+                login: 'Test',
+                first_name: 'FirstName',
+                last_name: 'LastName',
+                vk: {}
+            });
+            user.save((err, doc) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                user_id = doc._id;
+            })
+        });
+        
+        afterEach('Remove test user', function () {
+            User.remove({_id: user_id}, function (err) {
+                if (err) {
+                    console.log(err);
+                    return;
                 }
             })
-        })
-
-        let opts = {
-            url: "http://localhost:3007/api/playlists"
-        };
-        let id = "s:3MGdWbUNfrN7cyAyTcfDGLDL5IEqjwpk.BzuM2UW16FWOGJfD70EP+hw4Jss9/rcc6ovmpKZNQg4" //example id that I need
+        });
 
         it("Should return playlists", function (done) {
-            request.cookie(`connect.sid=${id}`);
+            let opts = {
+                url: "http://localhost:3007/api/playlists"
+            };
             request(opts, (err, res, body) => {
-                expect(res.statusCode).to.equal(200); //it works only when I copied connect.sid from a browser
+                expect(res.statusCode).to.equal(200); //User not found, cause session for this request is empty
                 console.log(body);
                 done();
             })
