88eb5a908b63cef7bf4de5ce1ebc1cc4e2b120a0
Graver75 <undergraver75@gmail.com>
2016-12-03 22:52:08 +0300

    module for getting session id

--- app/tests/tests.js
+++ app/tests/tests.js
@@ -3,8 +3,9 @@
  */
 "use strict";
 const _ = require('lodash');
-const request = require('request');
+const request = require('supertest')('http://localhost:3007'); //should i import port?
 const expect = require('chai').expect;
+const browser_cookies = require('browser-cookies');
 
 const User = require('./../models/user');
 
@@ -16,44 +17,46 @@ const commonController = require('./../controllers/common');
 let requestRaw = {
     session: null
 };
-let user_id;
+let sessionId;
 
 describe('Routes', function () {
-    describe('Get lists', function () {
-        beforeEach('Creates test user', function () {
-            let user = new User({
-                login: 'Test',
-                first_name: 'FirstName',
-                last_name: 'LastName',
-                vk: {}
-            });
-            user.save((err, doc) => {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-                user_id = doc._id;
-            })
+    before('Get session id', function () {
+        request.get('/api/users').expect(200).end( (err, res) => {
+            if (err) {
+                return console.log(err);
+            }
+            sessionId = res.headers['set-cookie'].pop().split(';')[0];
         });
+    });
 
-        afterEach('Remove test user', function () {
-            User.remove({_id: user_id}, function (err) {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-            })
+    beforeEach('Creates test user', function () {
+        let user = new User({
+            login: 'Test',
+            first_name: 'FirstName',
+            last_name: 'LastName',
+            vk: {}
         });
+        user.save((err, doc) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+        })
+    });
+
+    afterEach('Remove test user', function () {
+        User.remove({_id: user_id}, function (err) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+        })
+    });
+
+    describe('Get lists', function () {
 
         it("Should return playlists", function (done) {
-            let opts = {
-                url: "http://localhost:3007/api/playlists"
-            };
-            request(opts, (err, res, body) => {
-                expect(res.statusCode).to.equal(200); //User not found, cause session for this request is empty
-                console.log(body);
-                done();
-            })
+            
         });
     });
 });
