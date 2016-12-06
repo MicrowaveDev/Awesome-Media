33958b094ffe60036c28b6545453935da92973a0
Graver75 <undergraver75@gmail.com>
2016-12-06 20:05:15 +0300

    added connection to the database, bugs with session

--- app/tests/tests.js
+++ app/tests/tests.js
@@ -3,60 +3,73 @@
  */
 "use strict";
 const _ = require('lodash');
-const request = require('supertest')('http://localhost:3007'); //should i import port?
+const request = require('supertest');
 const expect = require('chai').expect;
-const browser_cookies = require('browser-cookies');
+const mongoose = require('mongoose');
 
 const User = require('./../models/user');
 
 const apiHelper = require('./../helpers/api');
-const playlistsController = require('./../controllers/playlists');
 const commonController = require('./../controllers/common');
 
-
-let requestRaw = {
-    session: null
-};
 let sessionId;
+let user_id;
+let root = "http://localhost"
+let port = 3007;
+
+request(`${root}:${port}`)
+    .get('/api/users').expect(200).end( (err, res) => {
+    if (err) {
+        return console.log(err);
+    }
+    sessionId = res.headers['set-cookie'].pop().split(';')[0];
+});
 
 describe('Routes', function () {
-    before('Get session id', function () {
-        request.get('/api/users').expect(200).end( (err, res) => {
-            if (err) {
-                return console.log(err);
-            }
-            sessionId = res.headers['set-cookie'].pop().split(';')[0];
-        });
-    });
 
-    beforeEach('Creates test user', function () {
-        let user = new User({
-            login: 'Test',
-            first_name: 'FirstName',
-            last_name: 'LastName',
-            vk: {}
-        });
-        user.save((err, doc) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-        })
-    });
-
-    afterEach('Remove test user', function () {
-        User.remove({_id: user_id}, function (err) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-        })
+    before('Open  connection to the DB', function (done) {
+        mongoose.connect('mongodb://localhost/awesome_media', done);
     });
 
     describe('Get lists', function () {
 
+        beforeEach('Creates test user', function (done) {
+            let user = new User({
+                login: 'Test',
+                first_name: 'FirstName',
+                last_name: 'LastName',
+                vk: {}
+            });
+            user.save((err, doc) => {
+                if (err) {
+                    return console.log(err);
+                }
+                user_id = doc._id;
+                done();
+            })
+        });
+
+        afterEach('Remove test user', function (done) {
+            User.remove({_id: user_id}, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                done()
+            })
+        });
+
         it("Should return playlists", function (done) {
-            
+            let req = request(`${root}:${port}`).get('/api/playlists');
+            req.cookies = sessionId;
+            req.set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end( (err, res) => {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    expect(res.body).to.be.a('array');
+                });
         });
     });
 });
